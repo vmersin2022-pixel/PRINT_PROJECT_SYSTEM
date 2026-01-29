0c4c1a88d6ff846d9c0bd17c3fb3efee7df80d8a
vmersin2022-pixel <vmersin2022@gmail.com>
2026-01-29 15:37:36 +0300

    feat: Use BrowserRouter instead of HashRouter
    Switches the application's routing mechanism from HashRouter to BrowserRouter to enable cleaner URLs.
    
    This change also updates vercel.json to properly rewrite all incoming requests to index.html for client-side routing on Vercel.

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AppProvider } from './context';
 
@@ -13,10 +13,10 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
